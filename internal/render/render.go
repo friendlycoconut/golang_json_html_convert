78440f7aeb5cfc3f0102a478bfd7c3572e07a2b8
friendlycoconut <kosten.il2000@gmail.com>
2025-07-31 19:08:07 +0200

    feat: added renderer to provide concurrency

---
+++ internal/render/render.go
@@ -0,0 +1,9 @@
+package render
+
+import "html/template"
+
+// Load parses the HTML template file once at startup.
+// The returned template is safe for concurrent Execute calls.
+func Load(path string) (*template.Template, error) {
+	return template.ParseFiles(path)
+}
