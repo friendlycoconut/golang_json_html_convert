ff8af226affc0231b5bf0a8de45c655445be6dc5
friendlycoconut <kosten.il2000@gmail.com>
2025-07-31 19:16:15 +0200

    feat: added render, that implements Single Responsibility

--- internal/render/render.go
+++ internal/render/render.go
@@ -4,6 +4,7 @@ import "html/template"
 
 // Load parses the HTML template file once at startup.
 // The returned template is safe for concurrent Execute calls.
+// Implements Single Responsibility
 func Load(path string) (*template.Template, error) {
 	return template.ParseFiles(path)
 }
